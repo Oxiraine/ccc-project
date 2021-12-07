022c3969bf57deb45ccd5f0a9466256b95d53d11
oxiraine <solaojo2882@yahoo.com>
2021-12-07 06:48:41 -0800

    intial commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var navLinks = document.getElementById("navLinks");
        
        function showMenu(){
-           navLinks.style.right = "0";
+           navLinks.style.left = "0";
        }
        function hideMenu(){
-           navLinks.style.right = "-200px";
+           navLinks.style.left = "-200px";
        }
